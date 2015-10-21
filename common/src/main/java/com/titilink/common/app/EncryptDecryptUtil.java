e5ab02c8335eb234ea866ffd17d8d20ef05bba02
ganting <tinggan@hotmail.com>
2015-10-21 10:38:42 +0800

    modify license

--- common/src/main/java/com/titilink/common/app/EncryptDecryptUtil.java
+++ common/src/main/java/com/titilink/common/app/EncryptDecryptUtil.java
@@ -1,9 +1,34 @@
 /**
- * 项目名称: titilink-framework
- * 文件名称: EncryptDecryptUtil.java
- * Date: 2015/8/11
- * Copyright: 2015 www.titilink.com Inc. All rights reserved.
- * 注意：本内容仅限于titilink公司内部传阅，禁止外泄以及用于其他的商业目的
+ * Copyright 2005-2015 titilink
+ *
+ * The contents of this file are subject to the terms of one of the following
+ * open source licenses: Apache 2.0 or LGPL 3.0 or LGPL 2.1 or CDDL 1.0 or EPL
+ * 1.0 (the "Licenses"). You can select the license that you prefer but you may
+ * not use this file except in compliance with one of these Licenses.
+ *
+ * You can obtain a copy of the Apache 2.0 license at
+ * http://www.opensource.org/licenses/apache-2.0
+ *
+ * You can obtain a copy of the LGPL 3.0 license at
+ * http://www.opensource.org/licenses/lgpl-3.0
+ *
+ * You can obtain a copy of the LGPL 2.1 license at
+ * http://www.opensource.org/licenses/lgpl-2.1
+ *
+ * You can obtain a copy of the CDDL 1.0 license at
+ * http://www.opensource.org/licenses/cddl1
+ *
+ * You can obtain a copy of the EPL 1.0 license at
+ * http://www.opensource.org/licenses/eclipse-1.0
+ *
+ * See the Licenses for the specific language governing permissions and
+ * limitations under the Licenses.
+ *
+ * Alternatively, you can obtain a royalty free commercial license with less
+ * limitations, transferable or non-transferable, directly at
+ * https://github.com/titilink/titilink-framework
+ *
+ * titilink is a registered trademark of titilink.inc
  */
 package com.titilink.common.app;
 
@@ -23,7 +48,8 @@ import java.security.spec.InvalidKeySpecException;
 /**
  * 加解密工具类
  *
- * @author ganting
+ * @author kam
+ * @date 2015/05/01
  * @since v1.0.0
  */
 public class EncryptDecryptUtil {
